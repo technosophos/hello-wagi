e6801e6a837ad2086cbc703c1816304cd7f02820
Matt Butcher <matt.butcher@microsoft.com>
2021-02-09 17:59:09 -0700

    update example to use routes
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 fn main() {
-    println!(
-        r#"Status: 200
-Content-Type: text/plain
-
-Hello World!
-"#
-    );
+    println!("Content-Type: text/plain\n\n Hello from main()");
 }
 
 // Use no_mangle so we can call this from WAGI or other external tools.
@@ -19,4 +13,17 @@ pub fn hello() {
 /// Another provider function that can be called directly.
 pub fn goodbye() {
     println!("Content-Type: text/plain\n\n Goodbye")
+}
+
+// This maps a few routes:
+// '/hello' will result in the `hello()` function being called.
+// '/goodbye' and all subpaths of '/goodbye' will call the `goodbye()` function.
+//
+// Note that when compiled, the `main` function is named `_start()`. So if you want
+// to map to that function, it is `/main _start`.
+#[no_mangle]
+pub fn _routes() {
+    println!("/hello hello");
+    println!("/goodbye/... goodbye");
+    println!("/main _start");
 }
